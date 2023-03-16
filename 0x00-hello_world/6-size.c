fc9fc61d5ee6ab8f896611c3cbe06827544edd59
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-16 06:20:46 -0700

    hh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ char k;
 float l;
 long int m;
 
-printf("Size of a char: %lu byte(s)\n" , (unsigned long)(k));
-printf("Size of a int: %lu byte(s)\n" , (unsigned long)(i));
-printf("Size of a long: %lu byte(s)\n" , (unsigned long)(j));
-printf("Size of a long int: %lu byte(s)\n" ,  (unsigned long)(m));
-printf("Size of a float: %lu byte(s)\n" ,  (unsigned long)(l));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)(k));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)(i));
+printf("Size of a long: %lu byte(s)\n", (unsigned long)(j));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)(m));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)(l));
 return (0);
 }
