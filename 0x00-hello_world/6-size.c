5171cf72c2a5262c7a38f3ff6dd899924fa70e5b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-16 14:08:09 +0100

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: sizeof "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+
+int i;
+long j;
+char k;
+float l;
+long int m;
+
+printf("Size of a char: %lu byte(s)\n" , (unsigned long)(k));
+printf("Size of a int: %lu byte(s)\n" , (unsigned long)(i));
+printf("Size of a long: %lu byte(s)\n" , (unsigned long)(j));
+printf("Size of a long int: %lu byte(s)\n" ,  (unsigned long)(m));
+printf("Size of a float: %lu byte(s)\n" ,  (unsigned long)(l));
+return (0);
+}
