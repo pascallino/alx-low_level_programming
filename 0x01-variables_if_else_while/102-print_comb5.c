17cc5c09ee4261bc368a19867f31754cd4aa2f0b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-17 08:44:42 -0700

    hh

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entyr point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a, b, c, i;
+
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(i);
+
+if (c == '9' && i == '8' && a == '9' && b == '9')
+{
+break;
+}
+if (!(c == '9' && i == '8' && a == '9' && b == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar((char) '\n');
+return (0);
+}
+
+
