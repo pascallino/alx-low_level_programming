62864d4f78f41bbbe22b259831b06684ebbd9f9f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-17 12:48:52 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and its less than 6 and not 0\n", n, m);
 }
 else if (m > 5)
 {
-printf("Last digit of %d is %d and its greater than 5\n", n, m);
+printf("Last digit of %d is %d and is greater than 5\n", n, m);
 }
 else if (m == 0)
 {
