ffeab74532b3dc3b1884630dd371a2707c237f6f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-17 13:03:41 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints alphabet in lowercase then uppercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+if (ch == 'a' || ch == 'e')
+{
+continue;
+}
+putchar(ch);
+	}
+putchar('\n');
+return (0);
+}
