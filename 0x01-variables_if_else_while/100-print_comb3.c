a5162bb1974df3bbb3add26932fd9db29ac0b744
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-17 04:01:37 -0700

    h

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints a-z
+ * Return: 0
+ */
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{
+putchar((char)i);
+putchar(',');
+putchar(' ');
+i++;
+}
+return (0);
+}
