84e76bc3743bb7c7f7efd7feda17b1c0ef186dc1
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-17 17:10:09 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entyr point
- * Description: prints two digits combination
- * Return: Always.  0 (success)
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-int a, b, c, i;
+	int c, i, k, j;
 
-for (a = '0'; a <= '9'; a++)
-{
-for (b = '0'; b <= '9'; b++)
-{
-for (c = '0'; c <= '9'; c++)
-{
-for (i = '0'; i <= '9'; i++)
-{
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
 
-if (((c + i) > (a + b) &&  c >= a) || a < c)
-{
-putchar(a);
-putchar(b);
-putchar(' ');
-putchar(c);
-putchar(i);
-
-if (c == '9' && i == '8' && a == '9' && b == '9')
-{
-break;
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-if (!(c == '9' && i == '8' && a == '9' && b == '9'))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-}
-putchar((char) '\n');
-return (0);
-}
-
