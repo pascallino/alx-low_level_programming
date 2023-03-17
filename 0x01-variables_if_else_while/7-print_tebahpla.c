6e2eaae9230c941a7189c3020a7c27450ab3a77b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-17 10:13:54 +0100

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints a-z
+ * Return: 0
+ */
+int main(void)
+{
+int i = 122;
+while (i <= 97)
+{
+putchar((char) i);
+i++;
+}
+putchar((char)'\n');
+return (0);
+}
