8e4a701855fa304d4a61d296f2e56d5d60e20b86
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-21 02:30:05 -0700

    real

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,54 +2,52 @@
 #include <stdio.h>
 
 /**
- * times_table - Entry point
- * Description: prints a-z
- * Return: 0
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
+ *
+ * my_putchar: prints the 9 times table
+ * @var: varholds the last digit
+ * Description2: last digit
  */
 
-void my_putchar(long var) {
-
-	/* print '-' for negative numbers */
-	if (var < 0) {
+void my_putchar(long var)
+{
+	if (var < 0)
+	{
 		_putchar('-');
 		var = var * -1;
 	}
-
-	/* Print Zero */
 	if (var == 0)
 		_putchar('0');
-
-	/* First remove the last digit of number and print 
-	   the remaining digits using recursion, then print
-	   the last digit
-	   */
 	if (var / 10)
-		my_putchar( var / 10);
-
-	_putchar(var%10 + '0');
+		my_putchar(var / 10);
+	_putchar(var % 10 + '0');
 }
 void times_table(void)
 {
+
 int i, j, t;
 long v;
 t = 0;
-for(i = 0; i <= 9; i++)
+
+for (i = 0; i <= 9; i++)
 {
-for(j = 0; j <= 9; j++)
+for (j = 0; j <= 9; j++)
 {
 v = i * j;
 if (j == 0)
 {
 _putchar('0');
 }
-else if (v <= 9)	
+else if (v <= 9)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(v + '0');
 }
-else 
+else
 {
 _putchar(',');
 _putchar(' ');
@@ -57,12 +55,7 @@ my_putchar(v);
 }
 
 }
-
 _putchar('\n');
-	}
 }
-int main(void)
-{
-	times_table();
-	return (0);
 }
+
