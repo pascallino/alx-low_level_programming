dffcee711c059761f42ce40a2f49adc4e75c7875
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-21 01:44:00 -0700

    good

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,25 +2,28 @@
 #include <stdio.h>
 
 /**
- * jack_bauer - Entry point
- * Description: prints a-z
- * Return: 0
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
  */
+
 void jack_bauer(void)
 {
+
 int i, j, k, l;
-for(i = 0; i <= 2; i++)
+
+for (i = 0; i <= 2; i++)
 {
-for(j = 0; j <= 3; j++)
+for (j = 0; j <= 3; j++)
 {
-for(k = 0; k <= 5; k++)
+for (k = 0; k <= 5; k++)
 {
-for(l = 0; l <= 9; l++)
+for (l = 0; l <= 9; l++)
 {
-_putchar(i + '0' );
+_putchar(i + '0');
 _putchar(j + '0');
 _putchar(':');
-_putchar(k + '0');;
+_putchar(k + '0');
 _putchar(l + '0');
 _putchar('\n');
 }
