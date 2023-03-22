fa55fa88b8c134792f9436df6c3e313a8b766015
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-22 11:42:47 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,23 +1,127 @@
-#include <stdio.h>
+
+0x03. C - Debugging
+C
+Debugging
+ By: Carrie Ybay
+ Weight: 1
+ Project will start Mar 21, 2023 6:00 AM, must end by Mar 24, 2023 6:00 AM
+ Checker was released at Mar 22, 2023 12:00 AM
+ An auto review will be launched at the deadline
+Resources
+Read or watch:
+
+Debugging
+Rubber Duck Debugging
+Debugging is the process of finding and fixing errors in software that prevents it from running correctly. As you become a more advanced programmer and an industry engineer, you will learn how to use debugging tools such as gdb or built-in tools that IDEs have. However, it’s important to understand the concepts and processes of debugging manually.
+
+
+
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+General
+What is debugging
+What are some methods of debugging manually
+How to read the error messages
+Copyright - Plagiarism
+You are tasked to come up with solutions for the tasks below yourself to meet with the above learning objectives.
+You will not be able to meet the objectives of this or any following project by copying and pasting someone else’s work.
+You are not allowed to publish any content of this project.
+Any form of plagiarism is strictly forbidden and will result in removal from the program.
+Requirements
+General
+Allowed editors: vi, vim, emacs
+All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+All your files should end with a new line
+Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+A README.md file at the root of the repo, containing a description of the repository
+A README.md file, at the root of the folder of this project (i.e. 0x03-debugging), describing what this project is about
+Quiz questions
+Great! You've completed the quiz successfully! Keep going! (Show quiz)
+Tasks
+0. Multiple mains
+mandatory
+In most projects, we often give you only one main file to test with. For example, this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project:
+
+carrie@ubuntu:/debugging$ cat main.c
+#include "main.h"
 
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	printf("Infinite loop incoming :(\n");
+        i = 98;
+        positive_or_negative(i);
 
-	i = 0;
-	/*
-	*while (i < 10)
-	*{
-	*	putchar(i);
-	*}
-	*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
+        return (0);
+}
+carrie@ubuntu:/debugging$
+carrie@ubuntu:/debugging$ cat main.h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
+carrie@ubuntu:/debugging$ 
+carrie@ubuntu:/debugging$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 positive_or_negative.c main.c
+carrie@ubuntu:/debugging$ ./a.out
+98 is positive
+carrie@ubuntu:/debugging$
+Based on the main.c file above, create a file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0.
+
+You are not coding the solution / function, you’re just testing it! However, you can adapt your function from 0x01. C - Variables, if, else, while - Task #0 to compile with this main file to test locally.
+
+You only need to upload 0-main.c and main.h for this task. We will provide our own positive_or_negative() function.
+You are not allowed to add or remove lines of code, you may change only one line in this task.
+carrie@ubuntu:/debugging$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 positive_or_negative.c 0-main.c -o 0-main
+carrie@ubuntu:/debugging$ ./0-main
+0 is zero
+carrie@ubuntu:/debugging$ wc -l 0-main.c
+16 1-main.c
+carrie@ubuntu:/debugging$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x03-debugging
+File: 0-main.c, main.h
+  
+1. Like, comment, subscribe
+mandatory
+Copy this main file. Comment out (don’t delete it!) the part of the code that is causing the output to go into an infinite loop.
+
+Don’t add or remove any lines of code, as we will be checking your line count. You are only allowed to comment out existing code.
+You do not have to compile with -Wall -Werror -Wextra -pedantic for this task.
+carrie@ubuntu:/debugging$ cat 1-main.c
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+      /*while (i < 10)
+       *{
+       *        putchar(i);
+       *}
+*/
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
 }
