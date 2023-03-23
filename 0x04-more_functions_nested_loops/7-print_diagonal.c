5a295361016778f0f63240624b7d945d26bdc6cd
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-23 05:24:43 -0700

    solved86

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,32 +1,33 @@
 #include "main.h"
 
-/**                                        * print_diagonal - Entry point
- * Description: checks is a character is u
-ppercase
+/**
+ * print_diagonal - Entry point
+ * Description: diagonal
  * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
 
 void print_diagonal(int c)
-{  
-    int size = c;
-
-if (!(c <= 0))
 {
-    for(int i = 0; i < size; i++)
-    {
-        for(int j = 0; j < size; j++)
-        {
-            if(j == i)
-                _putchar('\\');
-            else
-                _putchar(' ');
-        }
-        _putchar('\n');
-    }
-}
-    else 
-    {
-    _putchar('\n');
-    }
+	int size, i, j;
+
+	size = c;
+	if (!(c <= 0))
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
