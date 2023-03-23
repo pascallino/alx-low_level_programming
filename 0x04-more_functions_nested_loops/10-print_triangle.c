e817240a7eef56d1b4704c3abba156bfe66d488f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-23 04:47:11 -0700

    solved86

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * print_traingle - Entry point
- * Description: checks is a character is u
-ppercase
+ * print_triangle - Entry point
+ * Description: checks is a character is uppercase
  * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
 
 void print_triangle(int c)
-{  
-    int size = c;
-
-if (!(c <= 0))
 {
-    for(int i = 1; i <= size; i++)
-    {
-for(int j = 1; j <= size - i ; j++)
-        {
-                _putchar(' ');
-        }
-	for(int k = 1; k <= i ; k++)
-        {
-                _putchar('#');
-        }
-        _putchar('\n');
-    }
-}
-    else 
-    {
-    _putchar('\n');
-    }
+	int size, i, j, k;
+	size = c;
+
+	if (!(c <= 0))
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for ( j = 1; j <= size - i ; j++)
+			{
+				_putchar(' ');
+			}
+		for (k = 1; k <= i ; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
