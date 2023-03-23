4cabad153812bc6dad6778e94e08a4e52fd5981c
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-23 05:03:12 -0700

    solved86

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,28 +3,30 @@
 /**
  * print_square - Entry point
  * Description: checks is a character is u
-ppercase
+ ppercase
  * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
 
 void print_square(int c)
-{  
-    int size = c;
-
-if (!(c <= 0))
 {
-    for(int i = 0; i < size; i++)
-    {
-        for(int j = 0; j < size; j++)
-        {
-                _putchar('#');
-        }
-        _putchar('\n');
-    }
-}
-    else 
-    {
-    _putchar('\n');
-    }
+	int size, i, j;
+
+	size = c;
+
+	if (!(c <= 0))
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+			_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else 
+	{
+		_putchar('\n');
+	}
 }
