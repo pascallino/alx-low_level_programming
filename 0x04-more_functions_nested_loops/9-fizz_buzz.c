47a3b8a42ef69f48d8a200aa762d1cfa480aa09f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-23 04:55:12 -0700

    solved86

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,35 +9,35 @@
 int main(void)
 {
 	int a;
-	
+
 	printf("%d ", 1);
 	for (a = 2; a <= 100; a++)
 	{
-if (a % 3 == 0 && a % 5 == 0)
-{
-	printf("%s ", "FizzBuzz");
-	continue;
-}
-if (a % 3 == 0)
-{
-	printf("%s ", "Fizz");
-	continue;
-}
-if (a % 5 == 0)
-{
-	if (a == 100)
-	{
-printf("%s", "Buzz");
-continue;
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+			continue;
+		}
+		if (a % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+			continue;
+		}
+		if (a % 5 == 0)
+		{
+			if (a == 100)
+			{
+				printf("%s", "Buzz");
+				continue;
+			}
+			else
+			{
+				printf("%s ", "Buzz");
+				continue;
+			}
+		}
+		printf("%d ", a);
 	}
-	else
-	{
-printf("%s ", "Buzz");
-continue;
-	}
-}
-	printf("%d ", a);
-}
 	printf("%c", '\n');
-return (0);
+	return (0);
 }
