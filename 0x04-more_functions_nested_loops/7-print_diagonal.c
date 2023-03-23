9b065565edfab46b852b54a66ee8686f16ecb49b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-23 15:18:08 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,9 +14,9 @@ void print_diagonal(int c)
 	size = c;
 	if (!(c == 0))
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 0; j <= size; j++)
 			{
 if (i < j)
 {
