7b1fc8415a7a79a04a5c9885fc95dbe558493cb4
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-26 23:53:55 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - swap the a value to b
+ * @a: pointer parameter a
+ * @b: variable b
+ * Description: change pointer value
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int *swap;
+
+*swap = *a;
+*a = *b;
+*b = *swap;
+}
