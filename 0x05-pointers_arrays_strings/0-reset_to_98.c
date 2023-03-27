213fe5642cfeae38582515439ffe1c515144cf11
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-26 23:52:01 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset the n value to 98
+ * @n: pointer parameter
+ * Description: change pointer value
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers part 1
