7d7f9f040bc52380f839839dc1b3eecb354e3c7d
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-29 18:42:23 +0100

    New

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,7 @@
  * @src: The source string.
  * @n: The maximum number of bytes to copied from src.
  *
- * Return: A pointer to the resulting string dest.
+ * Return: A pointer to the new resulting string dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
