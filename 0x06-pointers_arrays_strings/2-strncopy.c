3cdbc3e8da29e070856b6fba957e76b644228e4b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-03-29 00:17:47 -0700

    good

--- 0x06-pointers_arrays_strings/2-strncopy.c
+++ 0x06-pointers_arrays_strings/2-strncopy.c
@@ -19,19 +19,19 @@ char *_strncpy(char *dest, char *src, int n)
 	i = 0;
 	j = 0;
 
-while (src[j] && j < n ) 
+while (src[j] && j < n)
 {
 dest[j] = src[j];
 j++;
 }
-if (n > j )
+if (n > j)
 {
 while (j <= n)
 {
 dest[j] = '\0';
 j++;
 }
-}                                      
+}
 return (dest);
 }
 
