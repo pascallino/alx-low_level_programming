8e10908705340a9b5b41dac54db8ac770be31b07
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-05 02:33:36 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,48 +1,47 @@
 #include <stdio.h>
 
-int root = 0;
-/**
- *setzero - set zero
- *Description:  function to reset root
- *Return: void
- */
-void setzero(void)
-{
-	root = 0;
-}
+int prime(int n, int loop);
 
-/**                                          * is_prime_number - Checks if a number is p
-rime and returns a number
+/**
+ * is_prime_number - Checks if a number is prime and returns a number
  * @n: The number to be checked.
+ * Description: Prime number
  * Return: If the integer is not prime - 0.
  * If the number is prime - 1.
  */
 
 int is_prime_number(int n)
 {
-int count;
-int loop;
+return (prime(n, 2));
+}
+
+/**
+ * prime - set prime
+ * @n: n varable
+ * @loop: loop var
+ * Description:  function to reset root
+ * Return: prime
+ */
+
+int prime(int n, int loop)
+{
+	int count;
+	int root;
 
 if (n <= 1)
 {
 	return (0);
 }
-loop = n;
-if (root == 0)
-{
+/* loop = n; */
 root = n;
-loop = 2;
-}
 count = (root - 1) / 2;
 if (root % loop == 0)
 {
-setzero();
 return (0);
 }
 if (loop == count && root % loop != 0)
 {
-setzero();
 return (1);
 }
-return (is_prime_number(loop + 1));
+return (prime(root, loop + 1));
 }
