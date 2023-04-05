8da942745447ada8b067d13298b94b768dff4698
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-05 02:11:31 -0700

    root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,51 +1,56 @@
 #include "main.h"
 #include <stdio.h>
 
+
+int  nroot(int n, int count);
+
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to return the square root of.
- *
+ *Description: recur sqrt func
  * Return: If n has a natural square root - the natural square root of n.
  *         If n does not have a natural square root - -1.
  */
 
-int root = 0;
-int count = 1;
-
-void setglobal(void)                    {                                       root = 0;;                              }
 
 int _sqrt_recursion(int n)
 {
-int count = n;
-int ans;
-int store;
 
-if (root == 0)
+	return (nroot(n, 1));
+}
+
+/**
+ * nroot - Returns the natural square root of a number.
+ * Description: helper function`
+ *@count: increment
+ * @n: The number to return the square root of n.
+ *Return: If n has a natural If n does not have a natural square root - -
+ */
+
+
+int nroot(int n, int count)
 {
- root  = n;
- count = 1;
-}
-if (root <=  0)
-{
-setglobal();
-return (-1);
-}
-ans = count * count;
-if (ans == root)
-{
-store = count;
-setglobal();
-        return (count);
-}
-else if (ans  > root)
-{                        
-	setglobal();
-	return (-1);       
-}
-else
-{
-return( _sqrt_recursion(count + 1));
-}
-setglobal();
-return (count);
+	int store = n;
+	int ans;
+	int root = n;
+
+	if (root <=  0)
+	{
+		return (-1);
+	}
+	ans = count * count;
+	if (ans == root)
+	{
+		store = count;
+		return (count);
+	}
+	else if (ans  > root)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (nroot(store, count + 1));
+	}
+	return (count);
 }
