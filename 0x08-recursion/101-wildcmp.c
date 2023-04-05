d1082010b7312039f86b1d1a654054f4272540f2
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-05 11:17:21 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * strlen_no_wilds - Returns all  the length of a string,
- * ignoring wildcard characters.
- * @str: The string to be measured.
- *
- * Return: The length.
+ * wildcmp - compares two strings.
+ * @s1: string 1.
+ * @s2: string 2. It can contains a * as a special character.
+ * Return: 1 if are identical, 0 if not.
  */
-
-int strlen_no_wilds(char *str)
+int wildcmp(char *s1, char *s2)
 {
-	int len = 0;
-	if (*str != '\0')
-	{
-		if (*str != '*')
-		{
-		len++;
-		}
-len = len + strlen_no_wilds(str + 1);
-	}
-return (len);
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
