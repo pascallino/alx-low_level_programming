c040c1af732667a78bd29086d166013cad21956c
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-07 07:16:53 -0700

    main add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,28 +14,28 @@ int main(int argc, char *argv[])
 int count, i, sum, len;
 char *s;
 
-	sum = 0;
-	if ((argc <  2))
-	{
-		printf("0\n");
-	}
+sum = 0;
+if ((argc <  2))
+{
+	printf("0\n");
+}
 else
 {
 	for (count = 1; count < argc; count++)
 	{
-s = argv[count];
-len = strlen(s);
-	for (i = 0; i < len; i++)
+		s = argv[count];
+		len = strlen(s);
+		for (i = 0; i < len; i++)
 		{
-	if ((isdigit(*(s + i)) == 0))
-		{
-			printf("Error\n");
-		return (1);
+			if ((isdigit(*(s + i)) == 0))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		sum += atoi(argv[count]);
+	}
+	printf("%d\n", sum);
 }
-			sum += atoi(argv[count]);
-		}
-		printf("%d\n", sum);
-}
-	return (0);
+return (0);
 }
