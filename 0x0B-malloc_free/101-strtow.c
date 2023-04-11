477935dca6dd439fa8fce9a1fb11d14492d98b46
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-11 13:18:31 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -24,13 +24,6 @@ char **strtow(char *str)
         return (NULL);
 
     /*Trim last space in the input string*/
-len = strlen(ptr);
-    while (len != 1 && ptr[len - 1] == ' ')
-    {
-        ptr[len - 1] = '\0';
-        len--;
-    }
-
     while (*ptr == ' ')
         ptr++;
 
