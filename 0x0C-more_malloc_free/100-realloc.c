1bb42f7e37bcab1870eb04bcfd56571ffea0968f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-13 12:53:25 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,12 +13,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *newptr;
 
-	if (new_size == 0)
+if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (ptr == NULL)
 	{
 		newptr = malloc(new_size);
