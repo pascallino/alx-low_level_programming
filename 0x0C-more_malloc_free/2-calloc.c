2b35b7ceb851478b8d46efc9f24490cdc1c99ac7
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-13 12:36:20 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,17 +5,17 @@
  * _calloc - Allocates memory for an array of a certain number of elements
  * @nmemb: The number of slots
  * @size: The byte size of each array slot
- * Return: If nmemb = 0 or if size = 0 return NULL,, or the function fails
+ * Return: If nmemb = 0 or if size = 0 return NULL,, or the function fails -NULL else return a pounter.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
 
-if (nmemb == 0 || size == 0)
+if (nmemb  < 1 || size < 1)
 {
 	return (NULL);
 }
-p = malloc(nmemb * size);
+p = calloc(nmemb, size);
 if (p == NULL)
 {
 	return (NULL);
