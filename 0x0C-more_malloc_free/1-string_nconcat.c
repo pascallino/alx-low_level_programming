cf2aa3603e6808fd8b2502d1cc86dd7ef49ff82c
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-13 08:02:47 -0700

    str

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,19 +7,19 @@
  * *string_nconcat - redirects the pointer to a new pointer witth a new address
  * @src: the second source string to append
  * @dest: the first  source of string
- * @lenght: number of characters to be copied
+ * @n: number of characters to be copied
  * Description: concat two strings
  * Return: a pointer
  */
 
 char *string_nconcat(char *dest, char *src, unsigned int n)
 {
-unsigned int dex;
+	unsigned int dex;
 	char *s;
-unsigned int len = n;
+	unsigned int len = n;
 
-dex = 0;
-if (dest == NULL)
+	dex = 0;
+	if (dest == NULL)
 	{
 		dest = "";
 	}
@@ -27,7 +27,7 @@ if (dest == NULL)
 	{
 		src = "";
 	}
-for (dex = 0; dest[dex]; dex++)
+	for (dex = 0; dest[dex]; dex++)
 		len++;
 
 
@@ -36,13 +36,13 @@ for (dex = 0; dest[dex]; dex++)
 	{
 		return (NULL);
 	}
-len = 0;
-for (dex = 0; dest[dex]; dex++)
+	len = 0;
+	for (dex = 0; dest[dex]; dex++)
 		s[len++] = dest[dex];
 
 	for (dex = 0; src[dex] && dex < n; dex++)
 		s[len++] = src[dex];
 
 	s[len] = '\0';
-return (s);
+	return (s);
 }
