23bb53f4f6f4ce421aa8315636619da2ecf15657
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-13 18:06:57 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -23,6 +23,25 @@ while (str[i])
 }
 
 /**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
  * main - Entry point
  * @argc: count
  * @argv: vector array
