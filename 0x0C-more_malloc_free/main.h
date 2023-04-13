acf75a6b2ea3a9347c0e9ef2e99826c3b1e11737
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-13 12:21:52 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: Pascal 
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc
- */
-
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -14,4 +14,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif
+
+#endif/*MAIN_H*/
