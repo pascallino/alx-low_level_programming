458acf79676404927fee29f27245298ce1399946
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-19 13:01:06 +0100

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -6,6 +6,17 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+/**
+ * struct opt - Struct opt object 
+ *
+ * @opt: The operator for calculation 
+ * @f: The function associated
+ */
+typedef struct opt
+{
+	char *opt;
+	int (*f)(int x, int y);
+} op_to;
 int (*get_op_func(char *s))(int, int);
 
 #endif /* 3_CALC_H */
