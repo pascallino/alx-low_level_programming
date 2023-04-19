6f061321a2a68a35726ed667722b672da7a4fd67
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-19 14:44:02 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,43 @@
-#include <stdio.h>
 #include "3-calc.h"
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
+
 /**
- * main - entry point
- * @argc: variable
- * @argv: variable
- * Return: value 0
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int t1, t2;
-	int (*ops)(int, int);
-	int result;
+	int num1, num2;
 	char *op;
 
-	if ((argc !=  4))
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	t1 = atoi(argv[1]);
-	t2 = atoi(argv[3]);
+
+	num1 = atoi(argv[1]);
 	op = argv[2];
-	if ((*op == '/' && t2 == 0) || (*op == '%' && t2 == 0))
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	ops = get_op_func(argv[2]);
-	if (get_op_func(argv[2]) == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	result = (*ops)(t1, t2);
-	printf("%d\n", result);
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
