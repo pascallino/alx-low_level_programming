bd1414d013da4894c68076ef327007fd853f055f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-19 13:26:25 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,23 +1,73 @@
 #include <string.h>
 #include "3-calc.h"
+#include <stdio.h>
 
-int op_add(int a, int b)
+/**
+  * op_div - div
+  * @a: ---
+  * @b: ---
+  *
+  * Return: ====
+  */
+int op_div(int a, int b)
 {
-	return (a + b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
 }
+/**
+  * op_sub - ===
+  * @a: ====
+  * @b: ===
+  *
+  * Return: ==-
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+  * op_mul - ===
+  * @a: -=-
+  * @b: =--
+  *
+  * Return: ===
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-int op_div(int a, int b)
-{
-	return (a / b);
-}
+
+/**
+  * op_mod - .===
+  * @a: ===
+  * @b: ===
+  *
+  * Return: ==-
+  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
+}
+/**
+  * op_add - ===
+  * @a: -==
+  * @b: ==
+  *
+  * Return: ====
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
 }
