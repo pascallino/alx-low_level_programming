769410935530b8558f64f090c7d2192b1b57c431
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-19 04:33:53 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,29 +4,30 @@
 /**
  * int_index - function that lookup an value
  * and returns its index
- * @array: pointer to the array elements
+ * @arr: pointer to the array elements
  * @size: size of array
  * @cmp: function pounter  to check index in array
  * if true return index  else -1 or 1
  * Return: Always index of type int (Success)
-*/
+ */
 
 
-int int_index(int *arr, int size, int (*cmp)(int)) 
+int int_index(int *arr, int size, int (*cmp)(int))
 {
-    int i;
-if (size <= 0)
-	return (-1);
+	int i;
 
-if (arr == NULL || cmp == NULL)
-	return (-1);
+	if (size <= 0)
+		return (-1);
 
-    for (i = 0; i < size; i++) 
-    {
-        if (cmp(arr[i]) != 0) 
+	if (arr == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-            return i;
-        }
-    }
-    return -1;  /*indicates that no element was found*/
+		if (cmp(arr[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);  /*indicates that no element was found*/
 }
