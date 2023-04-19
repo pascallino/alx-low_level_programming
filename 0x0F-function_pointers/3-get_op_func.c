8c594f194894ed97e9b6387dc337b01a07ad2f35
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-19 13:37:47 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,10 +4,6 @@
 int (*get_op_func(char *s))(int, int)
 {
 
-	if (s[0] != '+' || s[0] != '/' || s[0] != '-' || s[0] != '%' || s[0] == '*')
-	{
-return ((s[0] == '+') ? op_add : (s[0] == '-') ? op_sub : (s[0] == '/') ? op_div : (s[0] == '*') ? op_mul : op_mod);
 
-	}
 	return (NULL);
 }
