268018794bd8386174f6071bac57993c61a9be0b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-19 04:28:11 -0700

    codes£

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) 
+/**
+ * main - Entry point
+ * @argc: count the array
+ * @argv: array vectors
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
 {
 	int i;
 	int num_bytes;
 	unsigned char *code;
 
-	if (argc != 2) 
+	if (argc != 2)
 	{
-        printf("Error\n");
-        exit (1);
+		printf("Error\n");
+		exit(1);
 	}
 
 	num_bytes = atoi(argv[1]);
@@ -21,10 +28,10 @@ int main(int argc, char *argv[])
 		exit(2);
 	}
 	code = (unsigned char *)main;
-		for (i = 0; i < num_bytes; i++)
-		{
-			printf("%02x", code[i]);
-		}
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", code[i]);
+	}
 	printf("\n");
 	return (0);
 }
