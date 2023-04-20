ced485e3673188309210f5b8c02e56b45b2b020b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-20 12:47:47 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,54 +11,44 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i, sepind;
-	va_list args;
-	char *s;
+	int i;
+	int flag;
+	char *str;
+	va_list a_list;
 
+	va_start(a_list, format);
 	i = 0;
-	va_start(args, format);
-while (format != NULL && format[i] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
-		switch (*format)                        {
-		case 'i':
+		switch (format[i])
 		{
-			printf("%d", va_arg(args, int));
-			sepind = 0;
-			break;
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
 		}
-		case 's':
-		{
-			s = va_arg(args, char *);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s", s);
-			sepind = 0;
-			break;
-		}
-		case 'c':
-		{
-			printf("%c", va_arg(args, int));
-			sepind = 0;
-			break;
-		}
-		case 'f':
-		{
-			printf("%f", va_arg(args, double));
-			sepind = 0;
-			break;
-		}
-		default:
-		{
-			sepind = 1;
-			break;
-		}
-		}
-		if (format[i + 1] != '\0' && sepind == 0)
-		{
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
-		}
 		i++;
 	}
-	va_end(args);
 	printf("\n");
+	va_end(a_list);
 }
