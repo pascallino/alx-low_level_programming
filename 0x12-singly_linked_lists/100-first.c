6e1ea6d28fa66e7210ddfce0972379fa2168d33c
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-27 08:05:26 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void __attribute__((constructor)) intro_func(void);
 
 void intro_func(void)
 {
-	printf("You're beat! and yet,
-			you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
