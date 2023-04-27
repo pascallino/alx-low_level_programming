9973e873298e304bf3c68e6958f8fce4c3e798d5
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-27 06:30:39 -0700

    correct result

--- 0x12-singly_linked_lists/2-add_node_end.c
+++ 0x12-singly_linked_lists/2-add_node_end.c
@@ -13,6 +13,7 @@ list_t  *addNode(list_t** head_ref, const char *new_data)
 	/* Allocate memory for new node*/
 	list_t *new_node;
 	int len = 0;
+	list_t *last;
 
 	
 	new_node = (list_t*)malloc(sizeof(list_t));
@@ -28,13 +29,25 @@ list_t  *addNode(list_t** head_ref, const char *new_data)
     new_node->str = strdup(new_data);
     len = strlen(new_data);
     new_node->len = len;
-    new_node->next = *head_ref;
+    new_node->next = NULL;
     /* Set next pointer of new node to NULL as it will be added at the end*/
     /* If the linked list is empty, set the new node as the head of the list*/
-   
-*head_ref = new_node;
-
-
+    if (*head_ref == NULL) 
+    {
+	    *head_ref = new_node;
+	    return (*head_ref);
+    }
+    else
+    {
+    /* Traverse the linked list till the end*/
+	    last = *head_ref;
+	    while (last->next != NULL)
+	    {
+		    last = last->next;
+	    }
+    }
+    /* Add the new node at the end*/
+last->next = new_node;
 
     return (*head_ref);
 }
