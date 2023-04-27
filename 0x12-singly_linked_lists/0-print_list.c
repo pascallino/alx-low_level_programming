68be90ee564e63294448d2b0fb8868dbf244870a
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-27 16:43:17 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,12 @@ size_t print_list(const list_t *h)
 {
 size_t  i = 0;
 const list_t *curr = h;
-if (h == NULL)
-	return (0);
-
-
+	
 while (curr != NULL)
 {
 	if (curr->str == NULL)
 	{
-		printf("[0] %s\n", "(nil)");
+		printf("[0] (nil)\n");
 		curr = curr->next;
 	i++;
 	continue;
