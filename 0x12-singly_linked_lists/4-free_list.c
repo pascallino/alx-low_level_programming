23bfc3191a3186ef8b5344aaf55ed2868f61fa92
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-27 07:26:59 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - free allocsted space
+ * @head: pointer to head node
+ * Return: Number of nodes
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	list_t *current = head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+}
