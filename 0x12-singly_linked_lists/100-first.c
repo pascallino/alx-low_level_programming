48450f6644bfabc049e1c20bbb41803c943ccfb9
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-27 08:00:14 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 void __attribute__((constructor)) intro_func(void);
+/**
+ * intro_func - runs before main function is executed.
+ */
 
 void intro_func(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet,
+			you must allow,\nI bore my house upon my back!\n");
 }
