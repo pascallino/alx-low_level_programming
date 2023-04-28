44f67fc25b9706d3dd41f3af9a0bb0efbaf65d7f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-28 03:26:58 -0700

    add node end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,47 +6,60 @@
 /**
  * addNode - Add a node
  * @new_data: pointer to head node
- * Description: add new node
- * @head_ref: ===
+ * @head_ref: headnode
  * Return: Head node
  */
-listint_t  *addNode(listint_t **head_ref,
+
+listint_t *addNode(listint_t **head_ref,
 		const int new_data)
 {
 	/* Allocate memory for new node*/
 	listint_t *new_node;
-
+	listint_t *last;
 
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 /*	if (new_data == NULL)*/
-	/*{*/
-	/*	free(new_node);*/
-	/*	return (NULL);*/
-	/*}*/
+/*	{*/
+/*		free(new_node);*/
+/*		return (NULL);*/
+/*	}*/
 	/* Assign data to new node*/
 	new_node->n = new_data;
-	new_node->next = *head_ref;
+	new_node->next = NULL;
 	/* Set next pointer of new node to NULL as it will be added at the end*/
 	/* If the linked list is empty, set the new node as the head of the list*/
-
-	*head_ref = new_node;
-
+	if (*head_ref == NULL)
+	{
+		*head_ref = new_node;
+		return (*head_ref);
+	}
+	else
+	{
+		/* Traverse the linked list till the end*/
+		last = *head_ref;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+	}
+	/* Add the new node at the end*/
+	last->next = new_node;
 
 	return (*head_ref);
 }
 
 
 /**
- * add_nodeint - return the length of a linked list
- * @head: pointer to head node
- * @n: ======
+ * add_nodeint_end - return the length of a linked list
+ * @head: head node
+ * @n: string e
  * Return: Number of nodes
  */
 
-listint_t *add_nodeint(listint_t **head,
+listint_t *add_nodeint_end(listint_t **head,
 		const int n)
 {
 	return (addNode(head, n));
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,13 +18,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+listint_t *add_nodeint_end(listint_t **head, const int n);
 size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t list_len(const listint_t *h);
 listint_t *add_node(listint_t **head, const char *str);
-listint_t *add_node_end(listint_t **head, const char *str);
-listint_t *addNode(listint_t **head_ref, const char *new_data);
+listint_t *add_node_end(listint_t **head, const int str);
+listint_t *addNode(listint_t **head_ref, const int new_data);
 void free_list(listint_t *head);
 
 #endif /* LIST_H */
