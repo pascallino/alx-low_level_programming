05849df3970d321a43c959c3cb5256d08048eb5f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-04-29 16:10:57 -0700

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * reverse_listint - reverse a node
+ * @head: headnode
+ * Return: Head node
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	/* Allocate memory for new node*/
+	listint_t *prev = NULL;
+	listint_t *next = prev;
+
+	if (*head == NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (NULL);
+
+	/*	if (new_data == NULL)*/
+	/*	{*/
+	/*		free(new_node);*/
+	/*		return (NULL);*/
+
+	/* Traverse the linked list till the end*/
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	/* Add the new node at the end*/
+	*head = prev;
+
+	return (*head);
+}
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,5 +35,6 @@ listint_t *add_node_end(listint_t **head, const int str);
 listint_t *addNode(listint_t **head_ref, const int new_data);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* LIST_H */
