be138865897e98188eb84049b4db2fd8c15164f5
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-03 01:45:47 -0700

    done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -36,6 +36,6 @@ listint_t *addNode(listint_t **head_ref, const int new_data);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *reverse_listint(listint_t **head);
-listint_t *find_listint_loop(listint_t *head);	
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LIST_H */
