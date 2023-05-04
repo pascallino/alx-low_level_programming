a61709656567a507f9f7deeaf40be39655545f1f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-04 02:04:45 -0700

    0-bit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The number to be raised
+ * @y: The power
+ * Return: The value of x raised to the power of y.
+ */
+int _pow(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	result *= _pow(x, y - 1);
+	return (result);
+}
+
+/**
+ * binary_to_uint - binary to decimal
+ * @b: binary
+ * Description: binary conversion;
+ * Return: decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int n = 0, i = 0;
+	unsigned int val;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (b[i] != '\0')
+	{
+		if  (b[i] < '0' && b[i] > '1')
+		{
+			return (0);
+		}
+		i++;
+	}
+	i--;
+	val = atoi(b);
+	while (val  > 0)
+	{
+		num = num + ((val % 10)  *  _pow(2, n));
+		val = val / 10;
+		n++;
+	}
+	return (num);
+}
+
