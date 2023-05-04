3d05a5d1f005a286bd30d9f5fae9865a085b4974
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-04 13:20:35 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -31,14 +31,14 @@ unsigned int binary_to_uint(const char *b)
 	int n = 0, i = 0;
 	unsigned int val;
 
-	if (b == NULL)
+	if (!b)
 	{
 		return (0);
 	}
 
 	while (b[i] != '\0')
 	{
-		if  (b[i] < '0' && b[i] > '1')
+		if  (b[i] < 48 b[i] > 49)
 		{
 			return (0);
 		}
