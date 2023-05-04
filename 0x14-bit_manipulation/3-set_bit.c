cbf5eb6a7dd5cef66dd2e2b624520b199e215d3b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-04 03:13:29 -0700

    clear

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include "main.h"
-int set_bit(unsigned long int *n, unsigned int index) 
+
+/**
+ * set_bit - Sets the value of a bit to ON
+ * @n: A pointer
+ * @index: The index to set the value at - indices start at 0.
+ * Return: 1 else -1  If  error occurs
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
 {
     if (index >= sizeof(unsigned long int) * 8) 
     {
-        // index is out of range
+        /* index is out of range*/
         #ifdef DEBUG
         printf("Error: Index out of range\n");
         #endif
-        return -1;
+        return (-1);
     }
     *n |= 1UL << index;
-    return 1;
+    return (1);
 }
 
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include "main.h"
-int clear_bit(unsigned long int *n, unsigned int index) 
+/**
+ * clear_bit - clear  the value of a bit
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
+ * Return: 1 else -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8) 
-    {
-        #ifdef DEBUG
-        printf("Error: index out of range\n");
-        #endif
-        return -1;
-    }
-    *n &= ~(1UL << index);
-    return 1;
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+#ifdef DEBUG
+		printf("Error: index out of range\n");
+#endif
+		return (-1);
+	}
+	*n &= ~(1UL << index);
+	return (1);
 }
 
