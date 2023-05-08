6cfb157ee485b694387aa73274c79ccec784ec08
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-08 00:19:28 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,34 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * append_text_to_file - a funcion that appends text
+ * @filename: file to
+ * @text_content: contents to add
+ * Return: 1 on success, -1 on failure
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	FILE *file;
 	int result;
 
-    if (filename == NULL) 
-    {
-        return (-1);
-    }
-    if (text_content == NULL) 
-    {
-        return (1);
-    }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
 
-    file = fopen(filename, "a");
-    if (file == NULL) 
-    {
-        return (-1);
-    }
+	file = fopen(filename, "a");
+	if (file == NULL)
+	{
+		return (-1);
+	}
 
-    result = fputs(text_content, file);
+	result = fputs(text_content, file);
 
-    fclose(file);
+	fclose(file);
 
-    if (result == EOF) 
-    {
-        return (-1);
-    }
-    return (1);
+	if (result == EOF)
+	{
+		return (-1);
+	}
+	return (1);
 }
 
