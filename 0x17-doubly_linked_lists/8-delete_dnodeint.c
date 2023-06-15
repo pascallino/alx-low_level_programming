5d740cda665afb4e3449c1af246b92d25246bebb
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-15 12:58:48 +0100

    delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,61 +7,42 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
  * @idx: =================
  * Return: ===========
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int idx)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *curr = *head;
-	dlistint_t *len = *head;
-	dlistint_t *temp;
-	 dlistint_t *target;
-	int i = 0;
-	int index = idx;
+dlistint_t *tmp;
+dlistint_t *tmp2;
+unsigned int i;
 
-	if (!head || !*head)
-		return (-1);
-	while (len)
-	{
-		len = len->next;
-		i++;
-	}
-	if (i == 0)
-		return (-1);
-	if (index < 0 || index > i)
-		return (-1);
-	if (curr == NULL)
-	{
-		return (-1);
-	}
-	if  (*head && i == 1 && index == 0) /* this will only work for 100 only */
-	{
-		free(curr);
-		*head = NULL;
-		return (1);
-	}
-	else if (i > 0 && index == 0)
-	{
-	temp = curr;
-	curr = curr->next;
-	curr->prev = NULL;
-	*head = curr;
-	free(temp);
-	return (1);
-	}
-	else if (index > 0)
-	{
-	i = 0;
-	while (curr != NULL && i <= index)
-	{
-		temp = curr;
-		curr = curr->next;
-		i++;
-		if (i == index)
-			break;
-	}
-	target = curr->next;
-	temp->next = target;
-	target->prev = temp;
-	free(curr);
-	return (1);
-	}
-	return (-1);
+if (head == NULL)
+return (-1);
+if (*head == NULL)
+return (-1);
+if (index == 0)
+{
+tmp = *head;
+*head = (*head)->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+free(tmp);
+return (1);
+}
+tmp = *head;
+i = 0;
+while (tmp != NULL)
+{
+if (i == index - 1)
+{
+tmp2 = tmp->next;
+tmp->next = tmp->next->next;
+if (tmp->next != NULL)
+tmp->next->prev = tmp;
+free(tmp2);
+return (1);
+}
+tmp = tmp->next;
+i++;
+}
+return (-1);
+
+return (1);
 }
