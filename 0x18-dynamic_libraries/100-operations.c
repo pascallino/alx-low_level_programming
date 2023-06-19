eb964c73f139ddb4c69ff9c3ca95ba25417e79ff
pascallino <luvpascal.ojukwu@yahoo.com>
2023-06-18 23:41:36 -0700

    100-operations.so compiled

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+int add(int i, int k);
+int sub(int i, int k);
+int mul(int i, int k);
+int div(int i, int k);
+int mod(int i, int k);
+
+/**
+ * add - adds two numbers
+ * @i: num1
+ * @k: num2
+ * Return: result
+ */
+int add(int i, int k)
+{
+	return (i + k);
+}
+
+/**
+ * sub - ============
+ * @i: ================
+ * @k: =================
+ * Return: ===============
+ */
+int sub(int i, int k)
+{
+	return (i - k);
+}
+
+/**
+ * mul - ==============
+ * @i: ================
+ * @k: ===============
+ * Return: ============
+ */
+int mul(int i, int k)
+{
+	return (i * k);
+}
+
+/**
+ * div - ================
+ * @i: ==================
+ * @k: ================
+ * Return: ==============
+ */
+int div(int i, int k)
+{
+	return (i / k);
+}
+
+/**
+ * mod - ==========
+ * @i: ===============
+ * @k: ================
+ * Return: ==========
+ */
+int mod(int i, int k)
+{
+	return (i % k);
+}
