94dd38610291672761e1fd1682073531e3fac87b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-06 14:04:25 -0700

    get value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - hash item added to table
+ * @ht: hash tasble
+ * @key: key
+ * Return: value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			return (ht->array[i]->value);
+		}
+	}
+	return (NULL);
+
+}
