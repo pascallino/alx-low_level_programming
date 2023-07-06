908a3f3bdcd2074181453a23c60fb54b81c6835e
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-06 14:07:11 -0700

    get value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
