bd9c2e327db44641c4abbaee5328ace2d675626b
pascallino <luvpascal.ojukwu@yahoo.com>
2023-07-06 14:21:50 -0700

    get value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,20 +8,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index, i;
+	unsigned long int index;
+	hash_node_t *data;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
-	for (i = index; ht->array[i]; i++)
+	data = ht->array[index];
+	while (data)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			return (ht->array[i]->value);
-		}
-		return(NULL);
+		if (strcmp(data->key, key) == 0)
+			return (data->value);
+		data = data->next;
 	}
 	return (NULL);
 
