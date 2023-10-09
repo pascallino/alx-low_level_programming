e644e4cff54d217210feb1e23ec48f86fc0177cd
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-09 07:59:50 -0700

    0-linear

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,4 +14,5 @@ int _binary_search(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
 int b_search(int *array, size_t low, size_t high, int value);
 int advanced_binary(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
 #endif /*bend if */
