1e9383f35972d14cc9177408f889d8b90618d076
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-10 01:09:46 -0700

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -34,10 +34,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 int binary_search(int *array, size_t size, int value);
